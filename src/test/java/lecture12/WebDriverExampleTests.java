97da6aef9df39251cd09b4180e942797e0b5af1c
kaneva <karinaaneva93@gmail.com>
2023-06-12 15:51:49 +0300

    Lecture 10 - exercises

---
+++ src/test/java/lecture12/WebDriverExampleTests.java
@@ -0,0 +1,185 @@
+package lecture12;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class WebDriverExampleTests {
+    private static WebDriver driver;
+
+    @BeforeMethod
+    private static void setupDriver() {
+        System.out.println("==========Setting up WebDriver==========");
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions opt = new ChromeOptions();
+        opt.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(opt);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    private static void testMyFirstWebDriver() {
+        driver.get("http://training.skillo-bg.com:4300/posts/all");
+    }
+
+    @Test
+    private static void testFindElement() {
+        driver.get("http://training.skillo-bg.com:4200/posts/all");
+        WebElement login = driver.findElement(By.id("nav-link-login"));
+        login.click();
+    }
+
+    @Test
+    private static void testFindElement2() {
+        driver.get("http://training.skillo-bg.com:4300/posts/all");
+
+        //Finds Login link which you can use to interact with on a later stage
+        WebElement loginElement = driver.findElement(By.id("nav-link-login"));
+
+        //Finds Home link which you can use to interact with on a later stage
+        WebElement homeElement = driver.findElement(By.linkText("Home"));
+    }
+
+    @Test
+    private static void testFindElements() {
+        // vrashta prazen masiv, a ne greshka kato element
+        driver.get("http://training.skillo-bg.com:4300/posts/all");
+        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //Finds all posts elements
+        List<WebElement> posts = driver.findElements(By.xpath("//*[@class='post-feed-img']"));
+        System.out.println("The number of post elements is: " + posts.size());
+    }
+
+    @Test
+    private static void testClick() {
+
+        //predi da avtomatizirate, go prav ite rachno, zapoznatave se s detajlite rachno
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("http://training.skillo-bg.com:4300/posts/all");
+
+        WebElement login = driver.findElement(By.id("nav-link-login"));
+
+        login.click();
+    }
+
+    @Test
+    private static void testSendKeys() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+// раб с инпут, техт филд
+        driver.get("http://training.skillo-bg.com:4300/posts/all");
+
+        WebElement login = driver.findElement(By.id("nav-link-loginfsdf"));
+        login.click();
+
+        WebElement username = driver.findElement(By.name("usernameOrEmail"));
+
+        //type in username field
+        username.sendKeys("test");
+    }
+
+    @Test
+    private static void testClear() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("http://training.skillo-bg.com:4300/posts/all");
+
+        WebElement login = driver.findElement(By.id("nav-link-login"));
+        login.click();
+
+        WebElement username = driver.findElement(By.id("defaultLoginFormUsername"));
+
+        //type in username field
+        username.sendKeys("test");
+        username.clear();
+    }
+
+    @Test
+    private static void testCommonCommands() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("http://training.skillo-bg.com:4300/posts/all");
+
+        //return title of the page as a string
+        String titleOfThePage = driver.getTitle();
+        System.out.println("The title is: " + titleOfThePage);
+
+        WebElement login = driver.findElement(By.id("nav-link-login"));
+        login.click();
+        WebElement rememberMe = driver.findElement(By.xpath("//input[@formcontrolname='rememberMe']"));
+        rememberMe.click();
+
+        //return true if element is selected(checked)
+        System.out.println("Element is selected: " + rememberMe.isSelected());
+
+        WebElement signInButton = driver.findElement(By.id("sign-in-button"));
+        //return text of the field as a string
+        String buttonText = signInButton.getText();
+        System.out.println("The text of the element is: " + buttonText);
+
+        //return true if element is displayed on the page
+        System.out.println("The element is displayed: " + signInButton.isDisplayed());
+
+        //return true if element is enabled, and you can interact with it
+        System.out.println("The element is enabled: " + signInButton.isEnabled());
+    }
+
+    @Test
+    private static void testDropdowns() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.amazon.com/");
+
+        // Select value from dropdown
+        WebElement select = driver.findElement(By.id("searchDropdownBox"));
+        //select a ne Web Elemnt
+        Select dropdownCategory = new Select(select);
+        //dropdownCategory.selectByVisibleText("Books");
+        dropdownCategory.selectByValue("search-alias=arts-crafts-intl-ship");
+        //  dropdownCategory.selectByVisibleText("Books");
+    }
+
+    @Test
+    private static void implicitWait() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("http://training.skillo-bg.com:4300/posts/all");
+        WebElement goToLoginBtn = driver.findElement(By.id("nav-link-login"));
+        goToLoginBtn.click();
+    }
+
+    @Test
+    private static void explicitWait() {
+        driver.get("http://training.skillo-bg.com:4300/posts/all");
+        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement goToLoginBtn = driver.findElement(By.id("nav-link-login"));
+
+        // Wait for posts to not become visible
+        By allPostLocator = By.tagName("app-all-posts");
+        WebElement allPosts = webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(allPostLocator));
+        goToLoginBtn.click();
+
+        // Wait for posts to disappear
+        webDriverWait.until(ExpectedConditions.invisibilityOf(allPosts));
+    }
+
+    @AfterMethod
+    private static void closeDriver() {
+        System.out.println("==========Closing WebDriver session==========");
+        driver.close();
+    }
+}
